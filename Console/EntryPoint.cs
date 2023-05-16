a231c1680e98f0abc9d47c17f2ceb038cda28924
User <user@Users-Mac.local>
2023-05-16 21:32:40 +0900

    refactor: move Moq related code to a dedicated folder

--- Console/EntryPoint.cs
+++ Console/EntryPoint.cs
@@ -1,19 +1,20 @@
+using AutoFixture;
 using Moq;
 
 public static class EntryPoint
 {
     public static void TestMoq()
     {
-// Create a mock object and set up a method to return a value
+        // Create a mock object and set up a method to return a value
         var mock = new Mock<IMyInterface>();
         mock.Setup(x => x.MethodName()).Returns("Mocked result");
 
-// Create an instance of the class under test and pass the mock object, then call the method.
+        // Create an instance of the class under test and pass the mock object, then call the method.
         var mockedObject = mock.Object;
         var sut = new MyClass(mockedObject);
         var result = sut.MyMethod();
 
-// Verify that the method was called once
+        // Verify that the method was called once
         mock.Verify(x => x.MethodName(), Times.Once);
 
         Console.WriteLine(result);
